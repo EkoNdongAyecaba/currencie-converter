113945c291b9154d58c1b4f2907b1bb497ae92c0
Eko Ndong <ekondongayecaba@gmail.com>
2026-06-11 13:39:16 +0200

    Making sure the buttons trigger when modified

---
+++ index.js
@@ -0,0 +1,19 @@
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+function updateRate() {
+  console.log("called");
+}
+
+currencyFirstEl.addEventListener("change", updateRate);
+
+currencySecondEl.addEventListener("change", updateRate);
+
+worthFirstEl.addEventListener("input", updateRate);
